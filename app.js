edbd4de52ee555aafd006d09d1a2745b5e6e2d4c
JP Sauve <jpsauve@mobials.com>
2020-02-16 14:00:15 -0500

    adds basic handler for add command

--- app.js
+++ app.js
@@ -1,12 +1,33 @@
 const chalk = require('chalk');
-const yargs = require('yargs');
-
+const yargs = require('yargs').pkgConf('notes');
+const log = console.log;
+// add
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    builder: {
+        title: {
+            describe: 'Note title',
+            demand: false,
+            type: 'string',
+        },
+        body: {
+            describe: 'Note body',
+            demand: true,
+            type: 'string',
+        }
+    },
+    handler: function (argv) {
+        log(argv.title ? `Adding with title: ${argv.title}` : `Adding with lame title: ${argv.defaultTitle}`);
+        log('Body: ', argv.body);
+    }
+})
 // list
 yargs.command({
     command: 'list',
     describe: 'List the notes',
-    handler: function () {
-        console.log('listing notes');
+    handler: function (argv) {
+        log('Listing all notes: ', argv);
     },
 })
 
@@ -14,8 +35,14 @@ yargs.command({
 yargs.command({
     command: 'read',
     describe: 'Read a note',
-    handler: function () {
-        console.log('reading note');
+    builder: {
+        title: {
+            describe: 'Title of note to read',
+            demand: true,
+        },
+    },
+    handler: function (argv) {
+        log('Reading note: ', argv);
     },
 })
 
@@ -23,8 +50,14 @@ yargs.command({
 yargs.command({
     command: 'remove',
     describe: 'Remove a note',
-    handler: function () {
-       console.log('removing note');
+    builder: {
+        title: {
+            describe: 'Note title to remove',
+            demand: true,
+        }
+    },
+    handler: function (argv) {
+       log('removing note');
     },
 })
 yargs.parse()
